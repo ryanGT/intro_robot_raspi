45603e6f30280e78af5cab2aa7629c2f5d4b6988
Ryan Krauss <ryanwkrauss@gmail.com>
2016-07-02 16:39:14 -0400

    pwm oscope 2

--- pwm_test.py
+++ pwm_test.py
@@ -13,10 +13,10 @@ pi.set_mode(pwm_pin, pigpio.OUTPUT)
 pi.set_PWM_frequency(pwm_pin, 1000)
 pi.set_PWM_range(pwm_pin, 100)
 
-pi.set_PWM_dutycycle(pwm_pin,75)
+#pi.set_PWM_dutycycle(pwm_pin,75)
 #time.sleep(0.5)
 
-#pi.set_PWM_dutycycle(pwm_pin,25)
+pi.set_PWM_dutycycle(pwm_pin,25)
 #time.sleep(0.5)
 
 #cleanup
